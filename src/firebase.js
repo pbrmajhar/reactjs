9cf951d781c0d8d684eb3483cc557bdcc77ad37b
pbrmajhar <pbrmajhar@gmail.com>
2021-07-05 19:04:48 +0600

    firebase initialized

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ8MwWZOfgiRgkz_yLx3S2xI7dZEVp_-c",
+  authDomain: "ecom-a945b.firebaseapp.com",
+  projectId: "ecom-a945b",
+  storageBucket: "ecom-a945b.appspot.com",
+  messagingSenderId: "248710618201",
+  appId: "1:248710618201:web:a8f58232988dc8a4aea548",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleAuthProvider = firebase.auth.GoogleAuthProvider();
